ceb3e6fce2981c1aa0eec945efff1fa07cb82c03
yoshisaac <isfortner@gmail.com>
2024-10-22 23:24:52 -0400

    Initial

---
+++ server.js
@@ -0,0 +1,33 @@
+//
+
+//imports
+const express = require('express');
+const app = express()
+
+//constants
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//static files
+app.use(express.static('public'));
+app.use("/css", express.static(__dirname + '/public/css'))
+app.use("/js", express.static(__dirname + '/public/js'))
+app.use("/img", express.static(__dirname + '/public/img'))
+
+//set views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+//server code/file serving code
+app.get('', (req, res) => {
+    res.render('index', {text: 'das is cool'});
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {text: 'woah hello'});
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+ 
